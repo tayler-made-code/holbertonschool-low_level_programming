25dcd82a5f9295983b1b6ac1fa1bb2f6407aacb2
F-O-P <tayler.coon@gmail.com>
2023-01-26 08:53:10 -0800

    $commit_name

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	char d;
 	float e;
 
-	printf("Size of a char: %lu byte(s)", sizeof(d));
-	printf("Size of an int: %lu byte(s)", sizeof(a));
-	printf("Size of a long int: %lu byte(s)", sizeof(b));
-	printf("Size of a long long int: %lu bytes(s)", sizeof(c));
-	printf("Size of a float: %lu byte(s)", sizeof(e));
+	printf("Size of a char: %lu byte(s)\n", sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(b));
+	printf("Size of a long long int: %lu bytes(s)\n", sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", sizeof(e));
 	return (0);
 }
