a1f3f8dfcbb062f9a4c1de82dbe3d315b7ef1045
F-O-P <tayler.coon@gmail.com>
2023-01-26 08:51:28 -0800

    problem6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float e;
+
+	printf("Size of a char: %lu byte(s)", sizeof(d));
+	printf("Size of an int: %lu byte(s)", sizeof(a));
+	printf("Size of a long int: %lu byte(s)", sizeof(b));
+	printf("Size of a long long int: %lu bytes(s)", sizeof(c));
+	printf("Size of a float: %lu byte(s)", sizeof(e));
 	return (0);
 }
