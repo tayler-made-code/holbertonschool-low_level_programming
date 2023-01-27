7c979291c199bf42abd09c35d50cabdd1546807d
F-O-P <tayler.coon@gmail.com>
2023-01-27 14:59:56 -0800

    problem5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_sign - Entry point
+* @n: positive, 0, negative
+* Return: +, 0 , - based on value of int
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return ('>');
+	}
+	else if (n < 0)
+	{
+		return ('<');
+	}
+	else
+	{
+		return (0);
+	}
+}
