f6ed43150bb6c2ae073b9d2d76956e26ce89871c
F-O-P <tayler.coon@gmail.com>
2023-01-27 12:04:39 -0800

    problem2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+/*
+* file: 1-alphabet.c
+*
+*/
+#include "main.h"
+/**
+* print_alphabet_x10 - Entry point
+* Description: prints alphabet
+* Return 0
+*/
+void print_alphabet_x10(void)
+{
+	char i;
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	}
+	_putchar('\n');
+}
