57f05f7210dd861659aa2531d6aaeb8af61ce1e1
F-O-P <tayler.coon@gmail.com>
2023-01-29 09:09:29 -0800

    problem8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,11 +11,13 @@ void jack_bauer(void)
 	for (h = 0; h < 24; h++)
 	{
 		for (m = 0; m < 60; m++)
+		{
 			_putchar((h / 10) + '0');
 			_putchar((h % 10) + '0');
 			_putchar(':');
 			_putchar((m / 10) + '0');
 			_putchar((m % 10) + '0');
 			_putchar('\n');
+		}
 	}
 }
