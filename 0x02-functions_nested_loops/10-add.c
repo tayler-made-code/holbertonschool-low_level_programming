737a48c4a6b0ee947fb70acbb13225aeb07c04db
F-O-P <tayler.coon@gmail.com>
2023-01-29 20:49:02 -0800

    problem10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -9,17 +9,5 @@ int add(int x, int y)
 	int o;
 
 	o = (x + y);
-	if (o < 10)
-	{
-		return (o);
-	}
-	else if (o > 10)
-	{
-		_putchar((o / 10) + '0');
-		return ((o % 10) + 0);
-	}
-	else
-	{
-		return (0);
-	}
+	return (o);
 }
