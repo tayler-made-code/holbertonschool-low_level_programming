cb46bf605c80339b871cf1602b484d9045b6f080
F-O-P <tayler.coon@gmail.com>
2023-01-29 20:01:26 -0800

    problem10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* add - Entry point
+* @n: check last digit
+* Return: last digit only
+*/
+int add(int x, int y)
+{
+	int o;
+
+	o = (x + y);
+	if (o < 10)
+	{
+		return(o);
+	}
+	else if (o > 10)
+	{
+		_putchar((o / 10) + '0');
+		return((o % 10) + '0');
+	}
+	else
+	{
+		continue;
+	}
+}
