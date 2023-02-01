647c4752ea50e0f7e6d86f3c4e3b63391e587195
F-O-P <tayler.coon@gmail.com>
2023-02-01 09:41:54 -0800

    problem2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* mul - Entry point
+* @a: gets 1st int for multiplication
+* @b: gets 2nd int for multiplication
+* Return: a * b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
