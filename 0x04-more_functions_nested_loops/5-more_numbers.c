8119a45884b8ce8e6d3f1d201fcf96ff0559cfbd
F-O-P <tayler.coon@gmail.com>
2023-02-01 10:33:17 -0800

    problem5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* more_numbers - Entry point
+*
+* Return: 0..14 10 times.
+*/
+void more_numbers(void)
+{
+	int n, i;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9 && i <= 14)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
