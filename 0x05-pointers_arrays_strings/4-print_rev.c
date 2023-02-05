72a00beec82956271a6407642874f0a88aac858d
F-O-P <tayler.coon@gmail.com>
2023-02-05 06:59:07 -0800

    problem4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,5 +8,17 @@
 
 void print_rev(char *s)
 {
-	
+	int revcount = 0;
+
+	while (s[revcount] != '\0')
+	{
+		revcount++;
+	}
+
+	while (revcount - 1 >= 0)
+	{
+		revcount--;
+		_putchar(s[revcount]);
+	}
+	_putchar('\n');
 }
