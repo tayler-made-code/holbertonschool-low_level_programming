658bb1d60b32e51002a6e110978f4cd18920e11c
F-O-P <tayler.coon@gmail.com>
2023-02-05 12:00:33 -0800

    problem6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - Entry point
+ * @str: get string
+ * Return: s_r_n_ 
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		else
+		{
+			continue;
+		}
+	}
+	_putchar('\n');
+}
