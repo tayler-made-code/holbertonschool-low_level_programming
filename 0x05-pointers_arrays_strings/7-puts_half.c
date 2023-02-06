6339508abe6cdd665b0126bf351a89f47c729ed6
F-O-P <tayler.coon@gmail.com>
2023-02-06 12:00:45 -0800

    problem8

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -26,7 +26,7 @@ void puts_half(char *str)
 	while (str[n] != '\0')
 	{
 		_putchar(str[n]);
-	n++;
+		n++;
 	}
 	_putchar('\n');
 }
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_array - Entry point
+ * @a: get string
+ * @n: get number of elements
+ * Return: 
+ */
+
+void print_array(int *a, int n)
+{
+	int f1;
+
+	for (f1 = 0; f1 < n; f1++)
+	{
+		if (f1 == 0)
+		{
+			printf("%d", a[f1]);
+		}
+		else
+		{
+			printf(", %d", a[f1]);
+		}
+	}
+	printf("\n");
+}
