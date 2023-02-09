8bd49dfb8c6665b372c160e3bba5373b1f858a34
F-O-P <tayler.coon@gmail.com>
2023-02-09 08:38:26 -0800

    problem1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,14 +1,28 @@
 #include "holberton.h"
 
 /**
-* char *_strncat - Entry point
-* @dest:
-* @src:
-* @n:
-* Return:
+* *_strncat - Entry point
+* @dest: add src[n] to end of dest string
+* @src: get string
+* @n: get integer
+* Return: dest
 */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	
+	int i, d;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+
+	for (d = 0; d < n; d++)
+	{
+		dest[i + d] = src[d]
+		if (src[d] == '\0')
+		{
+			d = n;
+		}
+	}
+	return (dest);
 }
