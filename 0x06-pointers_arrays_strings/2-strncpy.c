cc8a98ac7cf42fc144aca89959c7a79587d5e9b5
F-O-P <tayler.coon@gmail.com>
2023-02-10 08:28:26 -0800

    problem2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,11 +12,6 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	if (*dest == '\0' || *src == '\0' || n == 0)
-	{
-		return (dest);
-	}
-
 	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
