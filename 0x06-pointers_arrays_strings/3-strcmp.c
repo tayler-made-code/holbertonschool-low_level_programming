42d60866ef34d642b84e8c27ad12891ccaccae61
F-O-P <tayler.coon@gmail.com>
2023-02-10 09:39:16 -0800

    problem3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,13 +1,25 @@
 #include "holberton.h"
 
 /**
-* _strcmp - Entry point
-* @s1:
-* @s2:
-* Return: int
+* _strcmp - compare two strings
+* @s1: get string 1
+* @s2: get string 2
+* Return: 0 if s1==s2, if not return -# or +# number based on ascii comparison
 */
 
-int *_strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
-	
+	int i;
+
+	for (i = 0; ; i++)
+	{
+		if (s1[i] == '\0' && s2[i] == '\0')
+		{
+			return (0);
+		}
+		else if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
 }
