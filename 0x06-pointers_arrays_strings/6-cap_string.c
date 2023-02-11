71c562ce5dbffaba09898d864a6b21bef684f7e7
F-O-P <tayler.coon@gmail.com>
2023-02-11 14:19:05 -0800

    problem6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,11 +2,29 @@
 
 /**
 * *cap_string - Entry point
-* @*:
-* Return: char
+* @str: get string
+* Return: String With Capitals
 */
 
-char *cap_string(char *)
+char *cap_string(char *str)
 {
-	
+	int i = 0;
+	int x;
+	char sep[] = {32, '\t', '\n', 33, 34, 40, 41, 44, 46, 59, 63, 123, 125};
+
+	while (str[i])
+	{
+		x = 0;
+
+		while (x < 13)
+		{
+			if ((i == 0 || str[i - 1] == sep[x]) && (str[i] >= 97 && str[i] <= 122))
+			{
+				str[i] = str[i] - 32;
+			}
+			x++;
+		}
+		i++;
+	}
+	return (str);
 }
