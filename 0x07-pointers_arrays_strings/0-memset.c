cbd3e991a57ae45196da38048021ba50273cecad
F-O-P <tayler.coon@gmail.com>
2023-02-12 13:17:52 -0800

    problem0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,16 +1,20 @@
 #include "main.h"
 
 /**
-* _memset - Entry point
-* @s: 
-* @b: 
-* @n: 
-* Return: dest
+* _memset - Fill the first n bytes of the memory pointed to by s with the constant byte b
+* @s: pointer to memory
+* @b: constant byte
+* @n: how many bytes to fill
+* Return: ptr value
 */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
-	
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
