ab352e4915b8f17f3db9f86f1801955373e252cb
F-O-P <tayler.coon@gmail.com>
2023-02-12 13:22:54 -0800

    problem0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _memset - Fill the first n bytes of the memory pointed to by s with the constant byte b
+* _memset - Fill the first n bytes of memory pointed to by s with bytes from b
 * @s: pointer to memory
 * @b: constant byte
 * @n: how many bytes to fill
