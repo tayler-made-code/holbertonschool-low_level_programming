e983a9e7029924695309338fd96b043d303c38be
F-O-P <tayler.coon@gmail.com>
2023-02-12 11:16:20 -0800

    problem0

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,12 +1,28 @@
 #include "holberton.h"
 
 /**
-* *leet - Entry point
-* @*:
-* Return: char
+* *leet - alter only specific characters in string
+* @str: get string
+* Return: r37urn s7ring
 */
 
-char *leet(char *)
+char *leet(char *str)
 {
-	
+	int i, x;
+	int seek[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int destroy[] = {'4', '3', '0', '7', '1'};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		x = 0;
+		while (x < 10)
+		{
+			if (str[i] == seek[x])
+			{
+				str[i] = destroy[x / 2];
+			}
+			x++;
+		}
+	}
+	return (str);
 }
