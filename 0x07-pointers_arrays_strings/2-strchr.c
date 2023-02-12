7fc5f0455278e268523de26643b9176c28b86df8
F-O-P <tayler.coon@gmail.com>
2023-02-12 14:21:22 -0800

    problem2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,14 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (*s == c)
+		if (s[i] == c)
 		{
-			return ((char *) s);
+			return (s + i);
 		}
-		s++;
 	}
 	return (NULL);;
 }
