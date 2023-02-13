20049df8406bcf5d59681e2afaf7ae6e91210216
F-O-P <tayler.coon@gmail.com>
2023-02-13 15:10:36 -0800

    $commit_title

--- 0x07-pointers_arrays_strings/5-main.c
+++ 0x07-pointers_arrays_strings/5-main.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-    char *s = "hello, world";
+    char *s = "hello, world hello there";
     char *f = "world";
     char *t;
 
--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,16 +1,27 @@
 #include "main.h"
 
 /**
-* _memset - Entry point
-* @s: 
-* @b: 
-* @n: 
-* Return: dest
+* _strstr - Entry point
+* @haystack: string to search in
+* @needle: substring to find
+* Return: needle if found or NULL if not
 */
 
-char *_memset(char *s, char b, unsigned int n)
+char *_strstr(char *haystack, char *needle)
 {
-	int i;
+	for (; *haystack != '\0'; haystack++)
+	{
+		char n = *needle, h = *haystack;
 
-	
+		while (h == n && n != '\0')
+		{
+			h++;
+			n++;
+		}
+		if (n == '\0')
+		{
+			return (haystack);
+		}
+	}
+	return ('\0');
 }
