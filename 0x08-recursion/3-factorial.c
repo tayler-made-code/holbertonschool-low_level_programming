ddc453b99757cfb7c6d91e1411fe4556285fbce9
F-O-P <tayler.coon@gmail.com>
2023-02-15 12:51:57 -0800

    problem3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
-* factorial - get the factorial of the int (ie, 5*4*3*2*1) 
+* factorial - get the factorial of the int (ie, 5*4*3*2*1)
 * @n: get int
 * Return: -1 if under 0, or the result of the factorial
 */
 
 int factorial(int n)
 {
-	if (n == 0) 
+	if (n == 0)
 	{
 		return (1);
 	}
@@ -18,6 +18,6 @@ int factorial(int n)
 	}
 	else
 	{
-		return (n * factorial(n-1));
+		return (n * factorial(n - 1));
 	}
 }
