9f70cb9f5dbb20ac38979078c84d985d4684db65
F-O-P <tayler.coon@gmail.com>
2023-02-15 08:07:23 -0800

    problem0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _puts_recursion - Add a description of the function
+* @s: get string
+* Return: string
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* FunctionName - Add a description of the function
+* @s: 
+* @b: 
+* Return: 
+*/
+
+void FunctionName(char *s)
+{
+	start coding here
+}
