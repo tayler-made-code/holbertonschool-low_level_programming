c7845087db64ed2490e41647eec11ed461b4d891
F-O-P <tayler.coon@gmail.com>
2023-02-16 10:05:15 -0800

    problem5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,13 +1,31 @@
 #include "main.h"
 
 /**
-* FunctionName - Add a description of the function
-* @s: 
-* @b: 
-* Return: 
+* sqrt2 - find the square root of n
+* @a: get int from _sqrt_recursion
+* @b: start with 1, reccursively increase by 1
+* Return: square root of n, or -1
+*/
+int sqrt2(int a, int b)
+{
+	if (b * b == a)
+	{
+		return (b);
+	}
+	else if (b * b > a)
+	{
+		return (-1);
+	}
+	return (sqrt2(a, b + 1));
+}
+
+/**
+* _sqrt_recursion - find the square root of n
+* @n: get int
+* Return: square root of n, or -1
 */
 
-void FunctionName(char *s)
+int _sqrt_recursion(int n)
 {
-	start coding here
+	return (sqrt2(n, 1));
 }
