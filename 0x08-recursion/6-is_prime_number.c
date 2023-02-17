4aaef338ce408e57b01d6ff0ccf178f11b0ee5f6
F-O-P <tayler.coon@gmail.com>
2023-02-17 07:57:59 -0800

    problem6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,13 +1,32 @@
 #include "main.h"
 
 /**
-* FunctionName - Add a description of the function
-* @s: 
-* @b: 
-* Return: 
+* prime - determine if a is divisible by b
+* @a: get int to check for prime
+* @b: get int for recursion
+* Return: 1 if prime, 0 if not, recurse until b = a
 */
 
-void FunctionName(char *s)
+int prime(int a, int b)
 {
-	start coding here
+	if (a == b)
+	{
+		return (1);
+	}
+	else if (b % a == 0)
+	{
+		return (0);
+	}
+	return (prime(a, b + 1));
+}
+
+/**
+* is_prime_number - determine if int is divisible by only itself and 1
+* @n: get int
+* Return: 1 if prime, 0 if not
+*/
+
+int is_prime_number(int n)
+{
+	return (prime(n, 2));
 }
