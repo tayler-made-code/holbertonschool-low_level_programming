b1f603cb48d99f4cc9777d4e0d7243ab6fb3662c
F-O-P <tayler.coon@gmail.com>
2023-02-17 09:10:46 -0800

    problem0

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+* _atoi - get - or + with string 
+* @s: get string
+* Return: atoi
+*/
+
+int _atoi(char *s)
+{
+	int i, j, ten2, neg, found;
+	unsigned int ten;
+
+	ten = 0;
+	i = 1;
+	neg = 1;
+	found = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j < i; j++)
+	{
+		if ((s[j] >= 48) && (s[j] <= 57))
+		{
+			ten = ten * 10 + (s[j] - '0');
+			found = 1;
+		}
+		else if (s[j] == 45)
+		{
+			neg = neg * -1;
+		}
+		else if (found == 1)
+		{
+			break;
+		}
+	}
+	ten2 = ten * neg;
+	return (ten2);
+}
