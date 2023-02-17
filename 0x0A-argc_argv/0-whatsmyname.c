da6d6236a317f89e5981fec44de01a5c20c0ed30
F-O-P <tayler.coon@gmail.com>
2023-02-17 10:00:08 -0800

    problem0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - print own name with a \n
+* @argc: number of arguments
+* @argv: array pointing to arguments
+* Return: 0
+*/
+
+int main(int argc, char**argv)
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+	return (0);
+}
