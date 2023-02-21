9f03b52766a9943a1ba24f4483e63dc12c4b012f
F-O-P <tayler.coon@gmail.com>
2023-02-20 16:38:48 -0800

    problem1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
 * _strdup - Add a description of the function
@@ -9,26 +10,26 @@
 
 char *_strdup(char *str)
 {
-	int i, end;
+	unsigned int i, end;
 	char *array;
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	for (end = 0; end <= *str; end++)
+	for (end = 0; str[end]; end++)
 	{
 	}
 	end += 1;
 	array = malloc(sizeof(char) * end);
 
-	for (i = 0; i < end; i++)
-	{
-		array[i] = str[i];
-	}
 	if (array == NULL)
 	{
 		return (NULL);
+	}
+	for (i = 0; i < end; i++)
+	{
+		array[i] = str[i];
 	}
 	return (array);
 }
