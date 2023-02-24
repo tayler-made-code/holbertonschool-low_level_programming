bc299e019adf1773fe1d069c769c3eea08abb8ae
F-O-P <tayler.coon@gmail.com>
2023-02-24 12:25:41 -0800

    problem3

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* _calloc - Add a description of the function
+* @nmemb: int
+* @size: size
+* Return: 0
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *arr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	arr = malloc(nmemb * size);
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		arr[i] = 0;
+	}
+	return (arr);
+}
