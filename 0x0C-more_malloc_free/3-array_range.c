4914460dba2031aefdc1dea146a1cc337b24214f
F-O-P <tayler.coon@gmail.com>
2023-02-24 12:39:30 -0800

    problem3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* array_range - Add a description of the function
+* @min: int
+* @max: size
+* Return: pointer to array
+*/
+
+int *array_range(int min, int max)
+{
+	int *arr;
+	int i, len;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	for (len = 0; len < (max - min); len++)
+	{
+	}
+	arr = malloc(sizeof(int) * (len + 1));
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= len; i++)
+	{
+		arr[i] = min++;
+	}
+	return (arr);
+}
