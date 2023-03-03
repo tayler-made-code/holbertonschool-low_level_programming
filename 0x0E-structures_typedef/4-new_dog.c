d337dc997e6810638fe73045c95717c087cc56fe
F-O-P <tayler.coon@gmail.com>
2023-03-03 08:44:36 -0800

    0x0E-P4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,8 +1,4 @@
 #include "dog.h"
-#include <stdlib.h>
-#include <stdio.h>
-int get_len(int i);
-char *str_cpy(char *dest, char *src);
 /**
  * new_dog - change dog info
  * @name: get dog name
@@ -13,42 +9,37 @@ char *str_cpy(char *dest, char *src);
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_name;
-	char *copy_name, *copy_owner;
-	unsigned int i, name_len = 0, owner_len = 0;
+	int i, name_len = 0, owner_len = 0;
 
-	new_name = malloc(sizeof(dog_t));
-	if (name == NULL)
+	new_name = (dog_t *) malloc(sizeof(dog_t));
+	if (new_name == NULL)
 		return (NULL);
 
-	if (name == NULL || age <= 0 || owner == NULL)
+	while (name[name_len])
+		name_len++;
+	new_name->name = (char *) malloc(name_len + 1);
+	if (new_name->name == NULL)
 	{
 		free(new_name);
 		return (NULL);
 	}
-	for (i = 0; name[i] != '\0'; i++)
-		name_len++;
-
-	for (i = 0; owner[i] != '\0'; i++)
-		owner_len++;
-
-	copy_name = malloc(sizeof(char) * (name_len + 1));
-	if (copy_name == NULL)
-		return (NULL);
-
-	copy_owner = malloc(sizeof(char) * (owner_len + 1));
-	if (copy_owner == NULL)
-		return (NULL);
-
 	for (i = 0; i <= name_len; i++)
 	{
-		copy_name[i] = name[i];
+		new_name->name[i] = name[i];
+	}
+	while (owner[owner_len])
+		owner_len++;
+	new_name = (char *) malloc(owner_len + 1);
+	if (new_name == NULL)
+	{
+		free(new_name->name);
+		free(new_name);
+		return (NULL);
 	}
 	for (i = 0; i <= owner_len; i++)
 	{
-		copy_owner[i] = owner[i];
+		new_name->owner[i] = owner[i];
 	}
-	new_name->name = copy_name;
-	new_name->owner = copy_owner;
 	new_name->age = age;
 	return (new_name);
 }
