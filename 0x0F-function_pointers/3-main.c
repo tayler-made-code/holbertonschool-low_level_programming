187851adf6de08fd7383eb7cb3d83cf7eb451f2f
F-O-P <tayler.coon@gmail.com>
2023-03-06 20:30:21 -0800

    0x0F-P3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,10 +15,14 @@ int main(int argc, char **argv)
 	if (argc != 4)
 		printf("Error\n"), exit(98);
 
-	if ((argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*' && argv[2][0] != '/' && argv[2][0] != '%') || strlen(argv[2]) != 1)
+	if ((argv[2][0] != '+' &&
+		argv[2][0] != '-' &&
+		argv[2][0] != '*' &&
+		argv[2][0] != '/' &&
+		argv[2][0] != '%') || strlen(argv[2]) != 1)
 		printf("Error\n"), exit(99);
 
-	if((argv[2][0] == '/' || argv[2][0] == '%') && (atoi(argv[3]) == 0))
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && (atoi(argv[3]) == 0))
 		printf("Error\n"), exit(100);
 
 	func = get_op_func(argv[2]);
