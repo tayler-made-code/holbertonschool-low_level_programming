19dbfd4c49a882d6a3d40d7a9c9d3f84024b42e4
F-O-P <tayler.coon@gmail.com>
2023-03-16 12:40:48 -0700

    0x12 - T4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_list - add a new node at the beginning singly linked list
+ * @head: point to pointer for head of singly linked list
+ * Return: address of the new element, or NULL if failed
+ */
+
+void free_list(list_t *head)
+{
+	if (head == NULL)
+		return;
+	else
+		free_list(head->next);
+	free(head->str);
+	free(head);
+}
