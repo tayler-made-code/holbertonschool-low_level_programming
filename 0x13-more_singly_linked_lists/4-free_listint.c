ecf5a68af4c21404ab19cdf200ba02f5086a21f8
F-O-P <tayler.coon@gmail.com>
2023-03-16 15:25:24 -0700

    0x13 - T4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees memory of list
+ * @head: pointer to head of list_t
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+		return;
+
+	while (head)
+	{
+		free(head);
+		if (head == NULL)
+			return;
+
+		head = head->next;
+	}
+}
