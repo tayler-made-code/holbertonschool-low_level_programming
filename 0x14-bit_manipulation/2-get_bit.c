6aa69d2403d4b0a20c608f71a7c85694ded50517
F-O-P <tayler.coon@gmail.com>
2023-03-20 11:07:26 -0700

    0x14 - T2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,12 +1,20 @@
 #include "main.h"
 
 /**
- * 
- * 
- * 
+ * get_bit - returns value of bit
+ * @n: base 10 number
+ * @index: index to print
+ * Return: value of bit or -1 for errors
  */
 
-unsigned int binary_to_uint(const char *b);
+int get_bit(unsigned long int n, unsigned int index)
 {
-	
+	unsigned int bits;
+
+	bits = (n >> index) & 1;
+
+	if ((bits == 1 || bits == 0))
+		return (bits);
+	else
+		return (-1);
 }
