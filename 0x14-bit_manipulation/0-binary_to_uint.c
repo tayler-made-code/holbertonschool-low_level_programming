ce60939f1b95e45bb98b5aa4cbb80a0f8e4747a6
F-O-P <tayler.coon@gmail.com>
2023-03-20 09:13:29 -0700

    0x14 - T0 - Fixed Betty compliance

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * binary_to_unit - converts binary to unsigned int
+ * binary_to_uint - converts binary to unsigned int
  * @b: pointer to a string of 0 and 1 characters
  * Return: converted number or 0 if NULL or has non-binary characters
  */
