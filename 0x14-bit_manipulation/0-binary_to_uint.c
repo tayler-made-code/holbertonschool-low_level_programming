b438704afdb993697771a2b668c00d6a24d8d537
F-O-P <tayler.coon@gmail.com>
2023-03-20 09:09:19 -0700

    0x14 - T0 fixed code

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,24 +8,17 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
-
-	int base = 1, i = 0;
+	unsigned int val = 0;
+	int i = 0;
 
 	if (b == NULL)
 		return (0);
 
-	while (b[i + 1])
+	while (b[i] == '0' || b[i] == '1')
 	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
+		val <<= 1;
+		val += b[i] - '0';
 		i++;
 	}
-	while (i >= 0)
-	{
-		result = result + ((b[i] - '0') * base);
-		base = base * 2;
-		i--;
-	}
-	return (result);
+	return (val);
 }
