563c854431e0b5f3d9332417eacb122ed6227aba
F-O-P <tayler.coon@gmail.com>
2023-03-20 11:14:51 -0700

    0x14 - T3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,12 +1,22 @@
 #include "main.h"
 
 /**
- * 
- * 
- * 
+ * set_bit - sets the value of a bit to 1 at a given index
+ * @n: base 10 number
+ * @index: index to set to 1
+ * Return: 1 if it worked, -1 if errors occur
  */
 
-unsigned int binary_to_uint(const char *b);
+int set_bit(unsigned long int *n, unsigned int index)
 {
-	
+	unsigned long int mask;
+
+	if (index > 63)
+		return (-1);
+
+	mask = 1 << index;
+
+	*n = *n | mask;
+
+	return (1);
 }
