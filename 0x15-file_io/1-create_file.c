763530dba1e0bb5ea96c3b5169f40d4a1f1d5979
F-O-P <tayler.coon@gmail.com>
2023-03-20 14:59:08 -0700

    0x14 - T1 fixed betty compliance issues

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 		check = write(file, text_content, i);
 
 		if (check == -1)
-			return(-1);
+			return (-1);
 	}
 	close(file);
 	return (1);
