9039022d74a1e887d094b9556a12a1b27a6826d5
F-O-P <tayler.coon@gmail.com>
2023-03-20 11:18:58 -0700

    0x14 - T4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,12 +1,22 @@
 #include "main.h"
 
 /**
- * 
- * 
- * 
+ * clear_bit - sets the value of a bit to 0 at index point
+ * @n: base 10 number
+ * @index: location of value to be changed
+ * Return: 1 if it worked, or -1 if errors occur
  */
 
-unsigned int binary_to_uint(const char *b);
+int clear_bit(unsigned long int *n, unsigned int index)
 {
-	
+	unsigned long int mask;
+
+	if (index > 63)
+		return (-1);
+
+	mask = 1 << index;
+
+	*n = *n & ~mask;
+
+	return (1);
 }
