95c434d55d55f9705af0a6f5dbe77a1c2215805c
F-O-P <tayler.coon@gmail.com>
2023-03-20 07:14:03 -0700

    0x13 - T9 - rewrite code to pass checks

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,35 +10,31 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int count = 0;
-
+	unsigned int i;
 	listint_t *new;
-	listint_t *temp;
+	listint_t *temp = *head;
 
-	if (*head == NULL)
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
 		return (NULL);
+	new->n = n;
 
-	temp = *head;
-
-	while (temp != NULL)
+	if (idx == 0)
 	{
-		if (count == idx - 1)
-		{
-			new = malloc(sizeof(listint_t));
-			if (new == NULL)
-				return (NULL);
-
-			else
-			{
-				new->n = n;
-				new->next = temp->next;
-				temp->next = new;
-				return (new);
-			}
-		}
-		if (temp->next != NULL)
-			temp = temp->next;
-		count++;
+		new->next = *head;
+		*head = new;
+		return (new);
 	}
-	return (NULL);
+	for (i = 0; temp != NULL && i < idx - 1; i++)
+		temp = temp->next;
+
+	if (temp == NULL || temp->next == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->next = temp->next;
+	temp->next = new;
+
+	return (new);
 }
