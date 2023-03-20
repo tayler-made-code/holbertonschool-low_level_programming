1877c8b0aabd9414ac2dbafe4be027ac34254568
F-O-P <tayler.coon@gmail.com>
2023-03-20 14:49:22 -0700

    0x14 - T0 & T1

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * read_textfile - 
+ *
+ *
+ *
+ *
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file, rd, wr;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+
+	if (file == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters + 1);
+	if (buffer == NULL)
+		return (0);
+
+	rd = read(file, buffer, letters);
+	if (rd == -1)
+		return (0);
+
+	buffer[letters] = '\0';
+
+	wr = write(1, buffer, rd);
+	if (wr == -1)
+		return (0);
+
+	close(file);
+	free(buffer);
+	return (wr);
+}
