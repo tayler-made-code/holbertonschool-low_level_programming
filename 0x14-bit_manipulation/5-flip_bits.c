88fe613ff36556aa0c10498db7a12afad17fcbbb
F-O-P <tayler.coon@gmail.com>
2023-03-20 11:27:25 -0700

    0x14 - T5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,12 +1,24 @@
 #include "main.h"
 
 /**
- * 
- * 
- * 
+ * flip_bits - returns the # of bits that would need to be
+ * flipped to get from one number to the other.
+ * @n: base 10 number
+ * @m: base 10 number
+ * Return: # of bits that would need to be flipped
  */
 
-unsigned int binary_to_uint(const char *b);
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	
+	unsigned long int i;
+	int count = 0;
+
+	i = n ^ m;
+
+	while (i != 0)
+	{
+		count += i & 1;
+		i = i >> 1;
+	}
+	return (count);
 }
