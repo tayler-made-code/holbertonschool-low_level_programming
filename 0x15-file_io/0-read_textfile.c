c2da658e1708efd877e510aa747e0dc33eb711bb
F-O-P <tayler.coon@gmail.com>
2023-03-20 20:19:53 -0700

    0x15 - T0 - Betty Fixes

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,6 +4,7 @@
  * read_textfile - reads a text file and prints it
  * @filename: name of the file
  * @letters: number of letters it should read and print
+ * Return: number of letters to print
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
