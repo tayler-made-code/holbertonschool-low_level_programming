d1397cdf3df3a7519e088a3eae280aad11775495
F-O-P <tayler.coon@gmail.com>
2023-03-24 09:19:09 -0700

    0x1A - T0 betty compliant, and updated .h in file

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,11 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - hash function implementing the djb2 algorithm
+ * @str: get str
+ * Return: hash
+ */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
