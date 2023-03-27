e26114444a0a4716dcd8e802bd604d4e6878a4e0
F-O-P <tayler.coon@gmail.com>
2023-03-27 12:57:16 -0700

    0x1A - T0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,6 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
-	unsigned long int i;
 
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
@@ -18,9 +17,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table->array = malloc(sizeof(hash_table->array) * size);
 	if (hash_table->array == NULL)
 		return (NULL);
-
-	for (i = 0; i < size; i++)
-		hash_table->array[i] = NULL;
 
 	return (hash_table);
 }
