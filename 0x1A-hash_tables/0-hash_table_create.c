181655f7d7ca7801f4b685c54c968d1f83b6d894
F-O-P <tayler.coon@gmail.com>
2023-03-27 11:02:12 -0700

    0x1A - T0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,10 +9,13 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
-
 	hash_table = malloc(sizeof(hash_table_t));
+
 	if (hash_table == NULL)
+	{
+		free(hash_table);
 		return (NULL);
+	}
 
 	hash_table->size = size;
 	hash_table->array = calloc(size, sizeof(hash_node_t *));
