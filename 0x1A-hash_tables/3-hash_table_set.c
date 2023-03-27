4272ed866661e85a8ab60265a40ef97008c9c850
F-O-P <tayler.coon@gmail.com>
2023-03-27 11:54:41 -0700

    0x1A - T3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,54 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - that adds an element to the hash table
+ * @ht: hash table to add or update the key/value to
+ * @key: key
+ * @value: value associated with the key
+ * value must be duplicated. value can be an empty string
+ *
+ * Return: 1 if success and 0 if failure
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *hnode = NULL, *collnode = NULL;
+	unsigned long int index;
+	char *valuedup = NULL, *keydup = NULL;
+
+	if (!ht || !(ht->array) || !key || !key[0])
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+	hnode = ht->array[index];
+	valuedup = strdup(value);
+	if (!valuedup)
+		return (0);
+
+	while (hnode != NULL)
+	{
+		if (!strcmp(hnode->key, key))
+		{
+			free(hnode->value);
+			hnode->value = valuedup;
+			return (1);
+		}
+		hnode = hnode->next;
+	}
+	hnode = ht->array[index];
+
+	collnode = malloc(sizeof(hash_node_t));
+	if (collnode == NULL)
+		return (free(valuedup), 0);
+
+	keydup = strdup(key);
+	if (!keydup)
+		return (free(valuedup), free(collnode), 0);
+
+	collnode->key = keydup;
+	collnode->value = valuedup;
+	collnode->next = hnode;
+	ht->array[index] = collnode;
+
+	return (1);
+}
