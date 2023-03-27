8f1c2f465e234ab520ae7a21e0048c1d5aec5635
F-O-P <tayler.coon@gmail.com>
2023-03-27 12:24:02 -0700

    0x1A - T4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: hash table to look at
+ * @key: key you are looking for
+ * Return: value associated with the element, NULL if not found
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (!ht || !key)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+	return (NULL);
+}
