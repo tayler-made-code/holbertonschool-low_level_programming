6e9900ccb38bd5a3a7776b9cc21354353ff7c7e8
F-O-P <tayler.coon@gmail.com>
2023-03-27 11:16:05 -0700

    0x1A - T2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,14 +1,22 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - hash function implementing the djb2 algorithm
+ * key_index - returns index of a key 
  * @key: key
- * @size: size of the array of the hash table
- * Return: the index at which the key/value pair should be stored in
- * the array of the hash table
+ * @size: size of array of the hash table
+ * Return: pointer to newly created hash table
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	
+	unsigned long int hash;
+	unsigned long int index;
+
+	if (key == NULL || size == 0)
+		return (0);
+
+	hash = hash_djb2(key);
+	index = hash % size;
+
+	return (index);
 }
