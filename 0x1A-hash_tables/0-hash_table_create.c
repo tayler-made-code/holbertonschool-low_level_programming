76f1ff9f80d7c24dd5aa7c62856affcd43980e7a
F-O-P <tayler.coon@gmail.com>
2023-03-27 10:56:01 -0700

    0x1A - T0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create a hash table
+ * @size: size of hash table
+ * Return: pointer to newly created hash table
+ */
+
+ hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table;
+
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+		return (NULL);
+
+	hash_table->size = size;
+	hash_table->array = calloc(size, sizeof(hash_node_t *));
+	if (!(hash_table->array))
+	{
+		free(hash_table);
+		return (NULL);
+	}
+	return (hash_table);
+}
