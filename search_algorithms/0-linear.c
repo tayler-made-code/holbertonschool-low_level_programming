7f486a924b99793145f59851dcc791aff0fa11d6
F-O-P <tayler.coon@gmail.com>
2023-12-14 17:50:19 -0600

    updated files

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,15 @@
+#include "search_algos.h"
+
+/**
+* _puts_recursion - Add a description of the function
+* @array: pointer to the first element of the array to search in
+* @size: the number of elements in the array
+* @value: the value to search for
+* Return: an index where value is located 
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+    if (array == NULL)
+        return (-1);
+}
---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,17 @@
+#include "search_algos.h"
+
+/**
+* _puts_recursion - Add a description of the function
+* @array: a pointer to the first element of the array to search in
+* @size: the number of elements in the array
+* @value: the value to search for
+* Return: the index where value is located
+*/
+
+
+
+int binary_search(int *array, size_t size, int value)
+{
+    if (array == NULL)
+        return (-1);
+}
