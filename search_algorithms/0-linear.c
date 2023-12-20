e0efb59a9f9c8260179dbbc063a27c08a8e76407
F-O-P <tayler.coon@gmail.com>
2023-12-20 14:03:10 -0600

    fixed betty styling, oh how i missed you, and also added print statements

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -5,18 +5,19 @@
 * @array: pointer to the first element of the array to search in
 * @size: the number of elements in the array
 * @value: the value to search for
-* Return: an index where value is located 
+* Return: an index where value is located
 */
 
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-  if (array == NULL)
-    return (-1);
-	
+	if (array == NULL)
+		return (-1);
+
 	for (i = 0; i < size; i++)
 	{
+	printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
