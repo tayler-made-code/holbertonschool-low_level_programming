cdbd65888b5fb23795f1a588a2931a866a49a75c
F-O-P <tayler.coon@gmail.com>
2023-12-20 13:55:22 -0600

    task 0, linear search

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -10,6 +10,15 @@
 
 int linear_search(int *array, size_t size, int value)
 {
+	size_t i;
+
     if (array == NULL)
         return (-1);
+	
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
