caf9985bea902d5f18b0800cb769ea1ae4ebcf76
F-O-P <tayler.coon@gmail.com>
2023-12-20 14:04:29 -0600

    updated function description

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
-* _puts_recursion - Add a description of the function
+* linear_search - Add a description of the function
 * @array: pointer to the first element of the array to search in
 * @size: the number of elements in the array
 * @value: the value to search for
