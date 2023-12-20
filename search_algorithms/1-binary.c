25f80e86b037629e72b6024cfee69470ef90d34e
F-O-P <tayler.coon@gmail.com>
2023-12-20 14:52:45 -0600

    binary search is working

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
-* _puts_recursion - Add a description of the function
+* binary_search - Add a description of the function
 * @array: a pointer to the first element of the array to search in
 * @size: the number of elements in the array
 * @value: the value to search for
@@ -9,9 +9,40 @@
 */
 
 
-
 int binary_search(int *array, size_t size, int value)
 {
-    if (array == NULL)
-        return (-1);
+	int lower = 0;
+	int upper = size - 1;
+	int i;
+
+	while (lower <= upper) {
+		int mid = (lower + upper)/ 2;
+
+		printf("Searching in array: ");
+		for (i = lower; i <= upper; i++) {
+			printf("%d", array[i]);
+			if (i != upper)
+				printf(", ");
+		}
+		printf("\n");
+
+
+		// Check if the value is present at the middle
+		if (array[mid] == value) {
+			return mid;
+		}
+
+		// If the value is smaller, ignore the right half
+		else if (array[mid] > value) {
+			upper = mid - 1;
+		}
+
+		// If the value is larger, ignore the left half
+		else {
+			lower = mid + 1;
+		}
+	}
+
+	// If the value is not present in the array
+	return -1;
 }
